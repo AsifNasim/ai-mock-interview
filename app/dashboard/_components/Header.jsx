53c9a68163700489dc86a9ecefd41c233cf37f94
anasim <anasim@egain.com>
2024-12-10 14:59:58 +0530

    wip: headers

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -1,17 +1,27 @@
+'use client'
 import { UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
-import React from 'react'
+import { usePathname } from 'next/navigation'
+import React, { useEffect } from 'react'
 
 const Header = () => {
+  const path = usePathname();
+  useEffect(() => {
+    console.log("Path Name -->", path)
+  }, [])
   return (
     <div className='flex p-4 items-center justify-between bg-secondary shadow-sm'>
       <Image src={'/ai-mock-interview.svg'} alt='ai-mock-interview' width={160} height={160} />
 
-      <ul className='flex gap-6'>
-        <li>Dashboard</li>
-        <li>Questions</li>
-        <li>Upgrade</li>
-        <li>How it works?</li>
+      <ul className='hidden md:flex gap-6'>
+        <li className={`hover:text-cyan-600 hover:font-bold transition-all cursor-pointer 
+          ${path === '/dashboard' && 'text-cyan-600 font-bold' }`}>Dashboard</li>
+        <li className={`hover:text-cyan-600 hover:font-bold transition-all cursor-pointer 
+          ${path === '/questions' && 'text-cyan-600 font-bold' }`}>Questions</li>
+        <li className={`hover:text-cyan-600 hover:font-bold transition-all cursor-pointer 
+          ${path === '/upgrade' && 'text-cyan-600 font-bold' }`}>Upgrade</li>
+        <li className={`hover:text-cyan-600 hover:font-bold transition-all cursor-pointer 
+          ${path === '/how' && 'text-cyan-600 font-bold' }`}>How it works?</li>
       </ul>
 
       <UserButton />
