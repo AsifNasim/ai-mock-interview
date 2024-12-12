9c8df221d8d538ce0b154fe91d0a33769fb7214e
anasim <anasim@egain.com>
2024-12-12 13:02:56 +0530

    Dialog box completed.

--- app/dashboard/_components/AddNewInterview.jsx
+++ app/dashboard/_components/AddNewInterview.jsx
@@ -1,6 +1,9 @@
 "use client"
 import React, { useState } from 'react'
 import { Button } from '@/components/ui/button';
+import { Input } from "@/components/ui/input"
+import { CANCEL, START_INTERVIEW, TELL_US_ABOUT_YOURSELF } from '@/utils/constants';
+
 import {
   Dialog,
   DialogClose,
@@ -10,41 +13,78 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
+import { Textarea } from '@/components/ui/textarea';
 
   
 
 const AddNewInterview = () => {
     const [openDialog, setOpenDialog] = useState(false);
+    const [jobRole, setJobRole] = useState(null);
+    const [jobDescription, setJobDescription] = useState(null);
+    const [yearsOfExperience, setYearsOfExperience] = useState(null);
+  
+  
+    const onSubmit =  (e) => {
+
+      e.preventDefault();
+      console.log("Job Role: ", jobRole);
+      console.log("Job Description: ", jobDescription);
+      console.log("Years of Experience: ", yearsOfExperience);
+    }
 
 
   return (
-    <div className='p-8 border rounded-lg bg-slate-100 hover:scale-105 hover:shadow-md cursor-pointer transition-all'
-    onClick={() => setOpenDialog(true)}
-    >
 
-        <Dialog>
-          <DialogTrigger className='font-bold text-lg text-center'>+ Add New</DialogTrigger>
-          <DialogContent>
-            <DialogHeader>
-              <DialogTitle>Are you absolutely sure?</DialogTitle>
-              <DialogDescription>
-                This action cannot be undone. This will permanently delete your account
-                and remove your data from our servers.
-                </DialogDescription>
-                <div className='flex gap-5 justify-end'>
-                  <DialogClose>
-                    <Button variant="ghost">Cancel</Button>
-                  </DialogClose>
-                  <Button>Start Interview</Button>
+    <div>
+      <div className='p-8 border rounded-lg bg-slate-100 hover:scale-105 hover:shadow-md cursor-pointer transition-all'
+          onClick={() => setOpenDialog(true)}>
+        <h2>+Add New</h2>
+      </div>
 
-                {/* <Button variant="ghost">Cancel</Button>
-                <Button>Start Interview</Button> */}
-                </div>
+      <Dialog open={openDialog} onOpenChange={setOpenDialog}>
+        {/* <DialogTrigger>Open</DialogTrigger> */}
+        <DialogContent className="max-w-2xl">
+          <DialogHeader>
+            <DialogTitle className="text-2xl">{TELL_US_ABOUT_YOURSELF}</DialogTitle>
+            <DialogDescription>
+              <form onSubmit={onSubmit}>
+                  <div>
+                  <h2>Add Details about your job/role, Job Description</h2>
+                  <div className='mt-5 '>
+                    <label >Job Role/Job Position</label>
+                    <Input onChange={(e) => setJobRole(e.target.value)}
+                    className="my-2" placeholder="Full Statck Developer, Backend Engineer etc." required />
+                  </div>
+
+                  <div className='mt-5 '>
+                    <label >Job Description/Tech Stack (In Short)</label>
+                    <Textarea onChange={(e) => setJobDescription(e.target.value)}
+                    className="my-2" placeholder="ReactJS, NodeJS, MySQL, Springboot etc" required/>
+                  </div>
+
+                  <div className='mt-5 '>
+                    <label >Years of experience</label>
+                    <Input onChange={(e) => setYearsOfExperience(e.target.value)}
+                    className="my-2" type="number" placeholder="4" min="0" max="30" required/>
+                  </div>
+
+                  </div>
+                  <div className='flex gap-5 justify-end'>
+                    <Button type="button" onClick={() => setOpenDialog(false )} variant="ghost">{CANCEL}</Button>
+                    <Button
+                     type="submit">{START_INTERVIEW}</Button>
+                  </div>
+              </form>
+
               
-            </DialogHeader>
-          </DialogContent>
-        </Dialog>
+            </DialogDescription>
 
+            
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+
+      
     </div>
   )
 }
---
+++ utils/constants.js
@@ -0,0 +1,4 @@
+export const ADD_NEW_INTERVIEW = 'ADD_NEW_INTERVIEW';
+export const CANCEL = 'Cancel';
+export const START_INTERVIEW = 'Start Interview';
+export const TELL_US_ABOUT_YOURSELF = 'Tell us more about your job you are interviewing';
